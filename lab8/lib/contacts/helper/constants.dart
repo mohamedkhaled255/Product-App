3378da7a2074bd60c419bad2e8724a614b34c6ac
mohamedkhaled255 <mohamedkha243e@gmail.com>
2025-08-20 19:20:51 +0300

    Delete lab8 directory

--- lab8/lib/contacts/helper/constants.dart
+++
@@ -1,5 +0,0 @@
-final String tableContacts = "UserContact";
-final String columnId = "id";
-final String columnName = "name";
-final String columnEmail = "email";
-final String columnPhone = "phone";
